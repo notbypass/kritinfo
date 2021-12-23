b5b50b7fd71e3807441f60ac2822e30a12d174f1
notbypass <79311349+notbypass@users.noreply.github.com>
2021-12-23 18:54:55 +0700

    Add files via upload

---
+++ KritInfo/SearchInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace KritInfo
+{
+    public partial class SearchInfo : Form
+    {
+        public SearchInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ListView1.Items.Clear();
+            //string query = "SELECT*FROM k_info where Godvipuska = '" + textBox1.Text + "';";
+            string query = "select * from kritinfo.k_info where concat(FIO, Groupstudent, Godvipuska, Phone, Region, Specialnost) like '%" + textBox1.Text + "%'";
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            MySqlCommand cmDB = new MySqlCommand(query, conn);
+            cmDB.CommandTimeout = 60;
+            try
+            {
+                conn.Open();
+                MySqlDataReader rd = cmDB.ExecuteReader();
+                if (rd.HasRows)
+                {
+                    while (rd.Read())
+                    {
+                        string[] row = { rd.GetString(0), rd.GetString(1), rd.GetString(2), rd.GetString(3), rd.GetString(4), rd.GetString(5), rd.GetString(6) };
+                        var listItem = new ListViewItem(row);
+                        ListView1.Items.Add(listItem);
+                    }
+                }
+                ListView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+    }
+
